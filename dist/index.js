1674e5996f2acd1a0c5b7f17b5eeae2605d8ca54
Lilli Szafranski <lilli@khanacademy.org>
2020-05-06 12:15:52 -0700

    Updating dist

--- dist/index.js
+++ dist/index.js
@@ -35,7 +35,7 @@ module.exports =
 /******/ 	// the startup function
 /******/ 	function startup() {
 /******/ 		// Load entry module and return exports
-/******/ 		return __webpack_require__(242);
+/******/ 		return __webpack_require__(9006);
 /******/ 	};
 /******/ 	// initialize runtime
 /******/ 	runtime(__webpack_require__);
@@ -2615,81 +2615,6 @@ function _loadMjsDefault() {
   });
   return _loadMjsDefault.apply(this, arguments);
 }
-
-/***/ }),
-
-/***/ 242:
-/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
-
-// @flow
-
-/**
- * This action runs `flow` and lints against uncovered expressions.
- *
- * It uses `send-report.js` to support both running locally (reporting to
- * stdout) and under Github Actions (adding annotations to files in the GitHub
- * UI).
- */
-
-// $FlowFixMe: shhhhh
-__webpack_require__(4285); // flow-uncovered-line
-
-const checkFile = __webpack_require__(6114);
-
-const sendReport = __webpack_require__(4836);
-const getBaseRef = __webpack_require__(7182);
-const gitChangedFiles = __webpack_require__(4885);
-
-async function run(flowBin) {
-    const files = await gitChangedFiles(await getBaseRef(), '.');
-    const jsFiles = files.filter((file) => file.endsWith('.js'));
-    if (!jsFiles.length) {
-        console.log('No changed files');
-        return;
-    }
-    const allAnnotations = [];
-    for (const file of jsFiles) {
-        const annotations = await checkFile(flowBin, file);
-        allAnnotations.push(...annotations);
-    }
-    await sendReport('Flow-coverage', allAnnotations);
-}
-
-// Deprecated -- included for backwards compatability with arcanist, until
-// we get completely off of phabricator.
-const runArcanist = async (flowBin, files) => {
-    //let failed = false; // MUSTDO (Lilli to Jared): Was there plans to do something with this variable or can we delete it? jared: "oh yeah I was gonna have the exit code match the failed status"
-    for (const filePath of files) {
-        const warnings = await checkFile(flowBin, filePath);
-        if (!warnings.length) {
-            continue;
-        }
-        //failed = true;
-
-        warnings.forEach((warning) => {
-            console.log(
-                `${warning.path}:::${warning.message}:::${warning.offset}`,
-            );
-        });
-    }
-};
-
-const [_, __, flowBin, ...argvFiles] = process.argv;
-
-if (flowBin) {
-    // flow-next-uncovered-line
-    run(flowBin).catch((err) => {
-        console.error(err); // flow-uncovered-line
-        process.exit(1);
-    });
-} else {
-    // arc lint is running us
-    run(process.env['INPUT_FLOW-BIN']).catch((err) => {
-        console.error(err); // flow-uncovered-line
-        process.exit(1);
-    });
-}
-
 
 /***/ }),
 
@@ -81085,42 +81010,6 @@ webpackEmptyAsyncContext.id = 4884;
 
 /***/ }),
 
-/***/ 4885:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
-// @flow
-const execProm = __webpack_require__(6738);
-const path = __webpack_require__(5622);
-const fs = __webpack_require__(5747);
-
-/**
- * This lists the files that have changed when compared to `base` (a git ref),
- * limited to the files that are a descendent of `cwd`.
- */
-const gitChangedFiles = async (
-    base /*:string*/,
-    cwd /*:string*/,
-) /*: Promise<Array<string>>*/ => {
-    cwd = path.resolve(cwd);
-    const {stdout, stderr} = await execProm(
-        `git diff --name-only ${base} --relative`,
-        {cwd, encoding: 'utf8', rejectOnError: true},
-    );
-    return (
-        stdout
-            .split('\n')
-            .filter(Boolean)
-            .map(name => path.join(cwd, name))
-            // Filter out paths that were deleted
-            .filter(path => fs.existsSync(path))
-    );
-};
-
-module.exports = gitChangedFiles;
-
-
-/***/ }),
-
 /***/ 4897:
 /***/ (function(__unusedmodule, exports, __webpack_require__) {
 
@@ -96481,256 +96370,6 @@ exports.transformFromAstAsync = transformFromAstAsync;
 
 /***/ }),
 
-/***/ 6114:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
-// @flow
-
-const {execSync} = __webpack_require__(3129);
-const path = __webpack_require__(5622);
-
-const fs = __webpack_require__(5747);
-
-// Allows text after comment explaining why
-const flowUncoveredLineRegex = /(\/\/\s*flow-uncovered-line[\s:]?.*|\/\*\s*flow-uncovered-line(\s+[^*]*)?\*\/)/;
-const flowNextUncoveredLineRegex = /(\/\/\s*flow-(next-uncovered|uncovered-next)-line|\/\*\s*flow-(next-uncovered|uncovered-next)-line(\s+[^*]*)?\*\/)/;
-
-const findIgnoredLinesAndPositions = (path, text) => {
-    const lines = text.split('\n');
-    const ignored /*: {[key: number]: boolean} */ = {};
-    const lineOffsets /*: {[key: number]: number} */ = {};
-    const ignoreBlocks = [];
-    const unmatchedBlocks = [];
-    let ignoring = false;
-    let next = false;
-    let numLines = 0;
-    let blockStart = 0;
-    lineOffsets[0] = 0;
-
-    lines.forEach((line, i) => {
-        i = i + 1; // one-indexed
-        lineOffsets[i] = lineOffsets[i - 1] + line.length + 1;
-        if (line.match(/^\s*\/\* flow-uncovered-block \*\//)) {
-            if (ignoring) {
-                unmatchedBlocks.push([blockStart, i - 1]);
-            }
-            ignoring = true;
-            blockStart = i;
-            return;
-        } else if (line.match(/^\s*\/\* end flow-uncovered-block \*\//)) {
-            if (!ignoring) {
-                throw new Error('unmatched end ignore pragma');
-            }
-            ignoring = false;
-            ignoreBlocks.push([blockStart, i]);
-            return;
-        }
-        if (next || flowUncoveredLineRegex.test(line)) {
-            next = false;
-            ignored[i] = true;
-        } else if (ignoring) {
-            ignored[i] = true;
-        }
-        next = flowNextUncoveredLineRegex.test(line);
-        numLines = i;
-    });
-    return {
-        unmatchedBlocks: unmatchedBlocks,
-        ignoredLines: ignored,
-        ignoredBlocks: ignoreBlocks,
-        numLines: numLines,
-        lineOffsets: lineOffsets,
-    };
-};
-
-const collectWarnings = (fileName, lineStats, uncoveredLocs) => {
-    const alreadyWarned /*: {[key: number]: boolean} */ = {};
-    const errorExists /*: {[key: number]: boolean} */ = {};
-    const warnings /*: Array<{
-        path: string,
-        start: {line: number, column: number},
-        end: {line: number, column: number},
-        annotationLevel: 'warning' | 'failure',
-        message: string,
-        offset: number,
-    }> */ = [];
-    const {
-        ignoredLines,
-        ignoredBlocks,
-        numLines,
-        lineOffsets,
-        unmatchedBlocks,
-    } = lineStats;
-    const threshold = 0.8;
-
-    unmatchedBlocks.forEach(([blockStart, blockEnd]) => {
-        warnings.push({
-            path: fileName,
-            start: {line: blockStart, column: 0},
-            end: {line: blockEnd, column: 0},
-            annotationLevel: 'failure',
-            message: `Unmatched /* flow-uncovered-block */`,
-            offset: lineOffsets[blockStart],
-        });
-    });
-
-    uncoveredLocs.forEach(({start, end}) => {
-        if (alreadyWarned[start.line]) {
-            return;
-        }
-        const isOneLine = start.line === end.line;
-        errorExists[start.line] = true;
-        if (isOneLine) {
-            if (!ignoredLines[start.line]) {
-                alreadyWarned[start.line] = true;
-                warnings.push({
-                    path: fileName,
-                    start,
-                    end,
-                    annotationLevel: 'failure',
-                    message: `The expression from ${start.line}:${
-                        start.column
-                    }-${
-                        end.column
-                        // Note that the `${''}` trick is so that it won't trip the regex that's looking for these comments
-                    } is not covered by flow! If it's unavoidable, put '// flow-${''}uncovered-line' at the end of the line`,
-                    offset: start.offset,
-                });
-            }
-        } else {
-            let ignored = true;
-            for (let i = start.line; i <= end.line; i++) {
-                if (!ignoredLines[i]) {
-                    ignored = false;
-                }
-                errorExists[i] = true;
-            }
-            if (!ignored) {
-                alreadyWarned[start.line] = true;
-                warnings.push({
-                    path: fileName,
-                    start,
-                    end,
-                    annotationLevel: 'failure',
-                    message: `The expression from ${start.line}:${
-                        start.column
-                    }-${end.line}:${
-                        end.column
-                    } is not covered by flow! If it's unavoidable, surround the expression in '/* flow-uncovered-block */' and '/* end flow-uncovered-block */'`,
-                    offset: start.offset,
-                });
-            }
-        }
-    });
-    let passable = 0;
-    let currentBlock /*: ?[number, number] */ = ignoredBlocks.shift();
-    for (let line = 1; line <= numLines; line++) {
-        if (ignoredLines[line] && !errorExists[line]) {
-            if (
-                currentBlock &&
-                currentBlock[0] <= line &&
-                line <= currentBlock[1]
-            ) {
-                passable += 1;
-            } else {
-                const offset = lineOffsets[line] - 1;
-                warnings.push({
-                    path: fileName,
-                    start: {line, column: 0},
-                    end: {line, column: 0},
-                    annotationLevel: 'failure',
-                    message: `The expression in line ${line} is covered by flow! You should remove any '// flow-${''}uncovered-line' or '/* flow-uncovered-block */' comments applying to this line.`,
-                    offset: offset,
-                });
-            }
-        }
-        if (currentBlock && line === currentBlock[1]) {
-            const blockLength = currentBlock[1] - currentBlock[0];
-            const offset = lineOffsets[currentBlock[0]] - 1;
-            if (blockLength > 0 && passable / blockLength > threshold) {
-                warnings.push({
-                    path: fileName,
-                    start: {line: currentBlock[0], column: 0},
-                    end: {line: currentBlock[1], column: 0},
-                    annotationLevel: 'failure',
-                    message: `More than ${Math.floor(
-                        threshold * 100,
-                    )}% of lines in the 'flow-uncovered-block' from lines ${
-                        currentBlock[0]
-                    }-${
-                        currentBlock[1]
-                    } are covered by flow! You should remove this comment from the entire block and instead cover individual lines using '// flow-${''}uncovered-line'.`,
-                    offset: offset,
-                });
-            }
-            passable = 0;
-            currentBlock = ignoredBlocks.shift();
-        }
-    }
-
-    return warnings;
-};
-
-/*::
-type CoverageInfo = {|
-        expressions: {|
-            uncovered_count: number,
-            covered_count: number,
-            uncovered_locs: $ReadOnlyArray<{|
-                start: {line: number, column: number, offset: number},
-                end: {line: number, column: number, offset: number},
-            |}>,
-        |},
-    |}
-*/
-
-const getCoverage = (flowBin, filePath) => {
-    const stdout = execSync(
-        path.resolve(flowBin) + ` coverage --json ${filePath}`,
-    ).toString('utf8');
-    const data /*: CoverageInfo */ = JSON.parse(stdout); // flow-uncovered-line
-
-    return data;
-};
-
-const isUncoveredFile = sourceText =>
-    sourceText.split('\n').includes('/* flow-uncovered-file */');
-
-const checkFile = (flowBin /*: string */, filePath /*: string */) => {
-    const sourceText = fs.readFileSync(filePath).toString('utf8');
-
-    if (isUncoveredFile(sourceText)) {
-        // Skipping this one
-        return [];
-    }
-
-    // not flow checked
-    if (!sourceText.includes('@flow')) {
-        return [];
-    }
-
-    const data = getCoverage(flowBin, filePath);
-    if (!data.expressions.uncovered_count) {
-        // All clear!
-        return [];
-    }
-
-    const ignoredLinesAndPositions = findIgnoredLinesAndPositions(
-        filePath,
-        sourceText,
-    );
-    return collectWarnings(
-        filePath,
-        ignoredLinesAndPositions,
-        data.expressions.uncovered_locs,
-    );
-};
-
-module.exports = checkFile;
-
-
-/***/ }),
-
 /***/ 6118:
 /***/ (function(module, __unusedexports, __webpack_require__) {
 
@@ -104849,76 +104488,6 @@ module.exports = function(hljs) {
     ]
   };
 };
-
-/***/ }),
-
-/***/ 7182:
-/***/ (function(module, __unusedexports, __webpack_require__) {
-
-// @flow
-/**
- * This is used to determine what the "base" branch for the current work is.
- *
- * - If the `GITHUB_BASE_REF` env variable is present, then we're running
- *   under Github Actions, and we can just use that. If this is being run
- *   locally, then it's a bit more tricky to determine the "base" for this
- *   branch.
- * - If this branch hasn't yet been pushed to github (e.g. the "upstream" is
- *   something local), then use the upstream.
- * - Otherwise, go back through the commits until we find a commit that is part
- *   of another branch, that's either master, develop, or a feature/ branch.
- *   TODO(jared): Consider using the github pull-request API (if we're online)
- *   to determine the base branch.
- */
-const execProm = __webpack_require__(6738);
-
-const getBaseRef = async (head /*:string*/ = 'HEAD') => {
-    if (process.env.GITHUB_BASE_REF) {
-        return `refs/remotes/origin/${process.env.GITHUB_BASE_REF}`;
-    } else {
-        let { stdout: upstream } = await execProm(
-            `git rev-parse --abbrev-ref '${head}@{upstream}'`,
-        );
-        upstream = upstream.trim();
-
-        // if upstream is local and not empty, use that.
-        if (upstream && !upstream.trim().startsWith('origin/')) {
-            return `refs/heads/${upstream}`;
-        }
-        let { stdout: headRef } = await execProm(
-            `git rev-parse --abbrev-ref ${head}`,
-        );
-        headRef = headRef.trim();
-        for (let i = 1; i < 100; i++) {
-            const { stdout } = await execProm(
-                `git branch --contains ${head}~${i} --format='%(refname)'`,
-            );
-            let lines = stdout.split('\n').filter(Boolean);
-            lines = lines.filter((line) => line !== `refs/heads/${headRef}`);
-
-            // Note (Lilli): When running our actions locally, we want to be a little more
-            // aggressive in choosing a baseRef, going back to a shared commit on only `develop`,
-            // `master`, feature or release branches, so that we can cover more commits. In case,
-            // say, I create a bunch of experimental, first-attempt, throw-away branches that
-            // share commits higher in my stack...
-            for (const line of lines) {
-                if (
-                    line === 'refs/heads/develop' ||
-                    line === 'refs/heads/master' ||
-                    line.startsWith('refs/heads/feature/') ||
-                    line.startsWith('refs/heads/release/')
-                ) {
-                    return line;
-                }
-            }
-        }
-        // If all else fails, just use upstream
-        return `${head}@{upstream}`;
-    }
-};
-
-module.exports = getBaseRef;
-
 
 /***/ }),
 
@@ -121666,6 +121235,104 @@ function get(object, path, defaultValue) {
 }
 
 module.exports = get;
+
+
+/***/ }),
+
+/***/ 9006:
+/***/ (function(__unusedmodule, __unusedexports, __webpack_require__) {
+
+// @flow
+
+/**
+ * This action runs `jest` and reports any type errors it encounters.
+ *
+ * It expects the path to the `jest` binary to be provided as the first
+ * argument, and it runs `jest` in the current working directory.
+ *
+ * It uses `send-report.js` to support both running locally (reporting to
+ * stdout) and under Github Actions (adding annotations to files in the GitHub
+ * UI).
+ */
+
+// $FlowFixMe: shhhhh
+__webpack_require__(4285); // flow-uncovered-line
+
+const sendReport = __webpack_require__(4836);
+const execProm = __webpack_require__(6738);
+
+async function run() {
+    const jestBin = process.env['INPUT_JEST-BIN'];
+    if (!jestBin) {
+        console.error(
+            `You need to have jest installed, and pass in the the jest binary via the variable 'jest-bin'.`,
+        );
+        process.exit(1);
+        return;
+    }
+    const {stdout} = await execProm(`${jestBin} --json`);
+    /* flow-uncovered-block */
+    const data /*:{
+        testResults: Array<{
+            name: string,
+            assertionResults: Array<{
+                status: string,
+                location: {line: number, column: number},
+                failureMessages: Array<string>,
+            }>,
+            message: string,
+            status: string,
+        }>,
+        success: bool,
+    }*/ = JSON.parse(
+        stdout,
+    );
+    /* end flow-uncovered-block */
+    if (data.success) {
+        console.log('All tests passed');
+        return;
+    }
+    const annotations = [];
+    for (const testResult of data.testResults) {
+        if (testResult.status !== 'failed') {
+            continue;
+        }
+        let hadLocation = false;
+        const path = testResult.name;
+        for (const assertionResult of testResult.assertionResults) {
+            if (
+                assertionResult.status === 'failed' &&
+                assertionResult.location
+            ) {
+                hadLocation = true;
+                annotations.push({
+                    path,
+                    start: assertionResult.location,
+                    end: assertionResult.location,
+                    annotationLevel: 'failure',
+                    message: assertionResult.failureMessages.join('\n\n'),
+                });
+            }
+        }
+        if (!hadLocation) {
+            console.log('no location,');
+            annotations.push({
+                path,
+                start: {line: 1, column: 0},
+                end: {line: 1, column: 0},
+                annotationLevel: 'failure',
+                message: testResult.message,
+            });
+        }
+    }
+    await sendReport('Jest', annotations);
+}
+
+// flow-next-uncovered-line
+run().catch(err => {
+    console.error(err); // flow-uncovered-line
+    process.exit(1);
+});
 
 
 /***/ }),
