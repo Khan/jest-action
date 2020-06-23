9921214e189c5adc8a3a95d48ec1015b3488e23d
Lilli Szafranski <lilli@khanacademy.org>
2020-06-23 16:23:54 -0700

    console

--- jest.js
+++ jest.js
@@ -28,6 +28,7 @@ async function run() {
         return;
     }
     const {stdout} = await execProm(`${jestBin} --json`);
+    console.log(stdout);
     /* flow-uncovered-block */
     const data /*:{
         testResults: Array<{
